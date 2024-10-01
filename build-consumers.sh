65c5de846114de7ac8d4efa18e1be8085d253cf1
Maker Zone <dev@makerzone.com.au>
2024-10-01 10:11:26 +1000

    Fixing build consumers to use dev branch of library

--- build-consumers.sh
+++ build-consumers.sh
@@ -3,6 +3,8 @@
 # Directory for consumers
 CONSUMERS_DIR="$HOME/ci-consumers"
 UNIVERSAL_MAKER_BOX_REPO="https://github.com/makers-multiverse/UniversalMakerBox.git"
+SENSOR_ANALOG_REPO="https://github.com/makers-multiverse/SensorAnalog.git"
+SENSOR_ANALOG_BRANCH="dev"  # Branch to use for SensorAnalog
 
 # Clean up any existing consumer directory
 if [ -d "$CONSUMERS_DIR" ]; then
@@ -19,9 +21,17 @@ git clone "$UNIVERSAL_MAKER_BOX_REPO" "$CONSUMERS_DIR/UniversalMakerBox"
 # Navigate to the UniversalMakerBox directory
 cd "$CONSUMERS_DIR/UniversalMakerBox" || exit
 
-# Run the build and tests for UniversalMakerBox
-echo "Building and running tests for UniversalMakerBox..."
+# Run prepare.sh to set up libraries (including default SensorAnalog)
+echo "Running prepare.sh for UniversalMakerBox..."
 bash prepare.sh
+
+# Overwrite SensorAnalog with the dev branch version
+echo "Overwriting SensorAnalog with dev branch..."
+rm -rf "$HOME/Arduino/libraries/SensorAnalog"  # Remove the default SensorAnalog
+git clone --branch "$SENSOR_ANALOG_BRANCH" "$SENSOR_ANALOG_REPO" "$HOME/Arduino/libraries/SensorAnalog"
+
+# Now run the consumer's build and tests
+echo "Building and running tests for UniversalMakerBox..."
 bash build.sh    # Or the appropriate build command for the consumer
 
 # Capture exit code to detect failure
@@ -31,3 +41,4 @@ if [ $? -ne 0 ]; then
 fi
 
 echo "Consumers prepared and tested successfully."
+
